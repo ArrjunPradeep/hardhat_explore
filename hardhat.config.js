688d93ddd737e57c865bfcc1c1a9498aaf6305bd
Arju <iaat@pop-os.localdomain>
2021-05-11 15:06:06 +0530

    hardhat_config_updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,15 @@
 
 require('@nomiclabs/hardhat-waffle') 
 
+const INFURA_URL = 'https://ropsten.infura.io/v3/';
+const PRIVATE_KEY = '';
 
 module.exports = {
   solidity: "0.8.0",
+  networks:{
+    ropsten: {
+      url:INFURA_URL,
+      accounts:[`0x${PRIVATE_KEY}`]
+    }
+  }
 };
